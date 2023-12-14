b90212b4482a0d9550e6990e393834e8e6e52c9a
zoy060 <mizan258173@gmail.com>
2023-12-15 00:43:38 +0600

    issue solve in book image view

--- book-list.php
+++ book-list.php
@@ -22,11 +22,13 @@ function bookList($bookList)
 
         for ($i = $n - 1; $i >= 0; $i--) {
                 echo '<form class="m-0 p-0 mt-3">      <button class="h-100 btn">';
-                echo    '<div class="card h-100 p-0 m-0">
-                        <img src="cover/1 (' . $n + 1 - $i . ').webp" class="card-img-top p-3 img" alt="">
-                        <div class="card-body">';
-                echo        '<h5 class="card-title">' . $bookList[$i]["Title"] . '</h5>';
-                echo         '   <p class="card-text">Author : ' . $bookList[$i]["Author"] . ' </p>
+                echo '<div class="card h-100 p-0 m-0">';
+
+                echo '<img src="cover/1 (' . ($n + 1 - $i) . ').webp" class="card-img-top p-3 img"> </img>';
+
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">' . $bookList[$i]["Title"] . '</h5>';
+                echo '<p class="card-text">Author : ' . $bookList[$i]["Author"] . ' </p>
                         </div>';
                 echo    '<div class="card-footer">
                             <small class="text-muted">Price : ' . $bookList[$i]['Price'] . ' tk</small>
